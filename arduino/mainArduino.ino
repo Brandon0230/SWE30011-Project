326ed76455477bf3951364dc0d73d8fccce02f86
Brandon0230 <103169140+Brandon0230@users.noreply.github.com>
2024-05-14 20:02:51 +1000

    update :3

--- arduino/mainArduino.ino
+++ arduino/mainArduino.ino
@@ -3,7 +3,6 @@
 
 const byte ROWS = 4; 
 const byte COLS = 4; 
-const char password[] = "1345";
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
@@ -24,10 +23,12 @@ int val = 0;
 const int rs = 45, en = 44, d4 = 43, d5 = 42, d6 = 41, d7 = 40;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 bool motionState = false; // We start with no motion detected.
-
-int currentState = 1; //O=Passcode, 1=Alcohol, 2=Open
+bool lcdNewPin = true;
+bool bacTextPrinted = false;
+int currentState = 0; //O=Passcode, 1=Alcohol, 2=Open
+String password = "1345";
 int bacLevel;
-
+String enteredPassword; //Array to store entered password
 void setup() {
   // Configure the pins as input or output:
   pinMode(PIRPin, INPUT);
@@ -71,9 +72,36 @@ void loop() {
 
 void togglePinPad() {
    char customKey = customKeypad.getKey();
-  
-  if (customKey){
-    Serial.print(customKey);
+  if (customKey == '1' || customKey == '2' || customKey == '3' || customKey == '4' || customKey == '5' || customKey == '6' || customKey == '7' || customKey == '8' || customKey == '9' || customKey == '0'){
+    enteredPassword += customKey;
+    Serial.println(enteredPassword);
+    if (lcdNewPin) {
+      lcd.clear();
+      lcdNewPin = false;
+    }
+    lcd.print("*");
+  }
+  if (customKey == 'C') {
+    enteredPassword = "";
+    lcd.clear();
+    lcd.print("Pin Cleared");
+    lcd.setCursor(0,1);
+    lcd.print("Please Enter Pin");
+    lcdNewPin = true;
+  }
+  if (customKey == 'D') {
+    
+  if (enteredPassword == password) {
+    currentState = 1;
+  }
+  else {
+    lcd.clear();
+    lcd.print("Incorrect Pin");
+    lcd.setCursor(0,1);
+    lcd.print("Please Try Again");
+     lcdNewPin = true;
+    enteredPassword = "";
+  }
   }
   // Read out the pirPin and store as val:
   val = digitalRead(PIRPin);
